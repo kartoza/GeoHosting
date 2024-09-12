3389fe72c15ddd7315ff81414c9195953ccfa402
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-09-12 12:26:26 +0200

    Update utils.ts

--- django_project/geohosting/src/utils/utils.ts
+++ django_project/geohosting/src/utils/utils.ts
@@ -10,21 +10,3 @@ export const setAxiosAuthToken = token => {
     delete axios.defaults.headers.common["Authorization"];
   }
 };
-
-
-export const getCurrencyBasedOnLocation = async () => {
-  try {
-    const response = await fetch('https://ipapi.co/json/');
-    const locationData = await response.json();
-    const userCountry = locationData.country_code;
-
-    let newCurrency = 'USD';
-    if (userCountry === 'ZA') newCurrency = 'ZAR';
-    else if (['AT', 'BE', 'FR', 'DE', 'IT', 'ES', 'NL', 'PT'].includes(userCountry)) newCurrency = 'EUR';
-
-    return newCurrency;
-  } catch (error) {
-    console.error('Error determining location or currency:', error);
-    return 'USD';
-  }
-};
