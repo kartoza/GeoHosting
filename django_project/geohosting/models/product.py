4ddf4e3889f0d02956d34c8ee062f972017eec8f
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-09-19 15:46:10 +0200

    Update product.py

--- django_project/geohosting/models/product.py
+++ django_project/geohosting/models/product.py
@@ -167,9 +167,7 @@ class ProductMedia(models.Model):
 
 @receiver(pre_save, sender=Product)
 def delete_old_image(sender, instance, **kwargs):
-    """
-    Delete the old image file if a new image is being set.
-    """
+    """Delete the old image file if a new image is being set."""
 
     if instance.pk:
         try:
