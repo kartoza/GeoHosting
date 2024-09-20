13fb7e92773ed3271b65e5592c2dab0eb1ceceda
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-09-20 15:37:45 +0200

    patch: update search text

--- django_project/geohosting/src/pages/DashboardPage/Support/SupportPage.tsx
+++ django_project/geohosting/src/pages/DashboardPage/Support/SupportPage.tsx
@@ -74,7 +74,7 @@ const SupportPage: React.FC = () => {
           onSearch={setSearchTerm}
           showDateFields={false}
           showClearButton={false}
-          placeholder={'Search by Order ID'}
+          placeholder={'Search Ticket'}
         />
         
       </Flex>
