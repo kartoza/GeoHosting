e1e5c2ef302f8667c51817aa3e45fc8dd82b5260
Irwan Fathurrahman <meomancer@gmail.com>
2024-11-21 14:57:19 +0700

    Fix flake

--- django_project/geohosting/tasks/erp.py
+++ django_project/geohosting/tasks/erp.py
@@ -4,7 +4,7 @@ from django.apps import apps
 
 @shared_task
 def sync_erp_data(class_name):
-    """Sync erp data from ERPNEXT API"""
+    """Sync erp data from ERPNEXT API."""
     if class_name:
         Model = apps.get_model('geohosting', class_name)
         Model.sync_data()
