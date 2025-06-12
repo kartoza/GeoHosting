fa83c2be8644e9bc6a3b94b5adda73e756f0f108
Irwan Fathurrahman <meomancer@gmail.com>
2025-06-12 09:42:54 +0700

    Add help to dashboard

--- django_project/geohosting/src/components/Help/index.tsx
+++ django_project/geohosting/src/components/Help/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { CSSProperties, FC } from "react";
 
 import {
   Button,
@@ -8,58 +8,68 @@ import {
   DrawerContent,
   DrawerOverlay,
   IconButton,
-  useDisclosure
+  useDisclosure,
 } from "@chakra-ui/react";
 import { FaQuestionCircle } from "react-icons/fa";
 import { DocsCrawlerPage } from "django-docs-crawler-react";
 
-import "django-docs-crawler-react/dist/style.css"
-import './index.scss';
+import "django-docs-crawler-react/dist/style.css";
+import "./index.scss";
 
 interface IHelp {
-  isDrawer: boolean;
+  isDrawer?: boolean;
+  backgroundColor?: string;
+  style?: CSSProperties | undefined;
 }
 
-const Help: FC<IHelp> = ({ isDrawer }) => {
+const Help: FC<IHelp> = ({
+  isDrawer,
+  backgroundColor = "gray.500",
+  style = {},
+}) => {
   const {
     isOpen: isDrawerOpen,
     onOpen: onDrawerOpen,
-    onClose: onDrawerClose
+    onClose: onDrawerClose,
   } = useDisclosure();
-  const relativeUrl = (window.location.pathname + window.location.hash).replace('/#', '')
+  const relativeUrl = (window.location.pathname + window.location.hash).replace(
+    "/#",
+    "",
+  );
 
   return (
     <>
-      {
-        isDrawer ?
-          <Button
-            aria-label="Open menu"
-            leftIcon={<FaQuestionCircle/>}
-            onClick={onDrawerOpen}
-            backgroundColor={'gray.500'}
-            _hover={{ backgroundColor: 'gray.500', opacity: 0.8 }}
-            minWidth={0}
-            marginLeft={isDrawer ? 0 : "-0.5rem"}
-          >Help</Button> :
-          <IconButton
-            aria-label="Open menu"
-            icon={<FaQuestionCircle/>}
-            onClick={onDrawerOpen}
-            backgroundColor={'gray.500'}
-            _hover={{ backgroundColor: 'gray.500', opacity: 0.8 }}
-            minWidth={0}
-            marginLeft={"-0.5rem"}
-          />
-      }
-      <Drawer
-        isOpen={isDrawerOpen} placement="right" onClose={onDrawerClose}
-      >
-        <DrawerOverlay/>
+      {isDrawer ? (
+        <Button
+          aria-label="Open menu"
+          leftIcon={<FaQuestionCircle />}
+          onClick={onDrawerOpen}
+          backgroundColor={backgroundColor}
+          _hover={{ backgroundColor: backgroundColor, opacity: 0.8 }}
+          minWidth={0}
+          marginLeft={isDrawer ? 0 : "-0.5rem"}
+          style={style}
+        >
+          Help
+        </Button>
+      ) : (
+        <IconButton
+          aria-label="Open menu"
+          icon={<FaQuestionCircle />}
+          onClick={onDrawerOpen}
+          backgroundColor={backgroundColor}
+          _hover={{ backgroundColor: backgroundColor, opacity: 0.8 }}
+          minWidth={0}
+          marginLeft={"-0.5rem"}
+        />
+      )}
+      <Drawer isOpen={isDrawerOpen} placement="right" onClose={onDrawerClose}>
+        <DrawerOverlay />
         <DrawerContent minWidth={"600px"}>
-          <DrawerCloseButton/>
+          <DrawerCloseButton />
           <DrawerBody>
             <DocsCrawlerPage
-              dataUrl={'/docs_crawler/data'}
+              dataUrl={"/docs_crawler/data"}
               open={true}
               setOpen={onDrawerClose}
               relativeUrl={relativeUrl}
@@ -68,7 +78,7 @@ const Help: FC<IHelp> = ({ isDrawer }) => {
         </DrawerContent>
       </Drawer>
     </>
-  )
-}
+  );
+};
 
-export default Help
+export default Help;
--- django_project/geohosting/src/components/Navbar/Navbar.tsx
+++ django_project/geohosting/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import {
   Box,
   Container,
@@ -14,15 +14,15 @@ import {
   Image,
   Link as ChakraLink,
   useDisclosure,
-  VStack
-} from '@chakra-ui/react';
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
-import { HamburgerIcon } from '@chakra-ui/icons';
-import LoginForm from '../LoginForm/LoginForm';
+  VStack,
+} from "@chakra-ui/react";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import LoginForm from "../LoginForm/LoginForm";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../redux/store";
 import { fetchProducts } from "../../redux/reducers/productsSlice";
-import { FaUser } from 'react-icons/fa'
+import { FaUser } from "react-icons/fa";
 import Help from "../Help";
 
 interface NavbarContentProps {
@@ -31,8 +31,8 @@ interface NavbarContentProps {
 }
 
 const STYLES = {
-  linkHovered: { textDecoration: "none", opacity: 0.8 }
-}
+  linkHovered: { textDecoration: "none", opacity: 0.8 },
+};
 
 /** Content of navbar
  * @param onOpen
@@ -41,117 +41,107 @@ const STYLES = {
 const NavbarContent: React.FC<NavbarContentProps> = ({ onOpen, isDrawer }) => {
   const navigate = useNavigate();
   const { token } = useSelector((state: RootState) => state.auth);
-  const { products } = useSelector(
-    (state: RootState) => state.products
-  );
-  return <>
-    <ChakraLink
-      as={RouterLink}
-      to="https://kartoza.com"
-      fontSize="md"
-      _hover={STYLES.linkHovered}
-      target='_blank'
-    >
-      Kartoza
-    </ChakraLink>
-    <ChakraLink
-      as={RouterLink}
-      to="https://kartoza.com/about"
-      fontSize="md"
-      _hover={STYLES.linkHovered}
-      target='_blank'
-      whiteSpace='nowrap'
-    >
-      About us
-    </ChakraLink>
+  const { products } = useSelector((state: RootState) => state.products);
+  return (
+    <>
+      <ChakraLink
+        as={RouterLink}
+        to="https://kartoza.com"
+        fontSize="md"
+        _hover={STYLES.linkHovered}
+        target="_blank"
+      >
+        Kartoza
+      </ChakraLink>
+      <ChakraLink
+        as={RouterLink}
+        to="https://kartoza.com/about"
+        fontSize="md"
+        _hover={STYLES.linkHovered}
+        target="_blank"
+        whiteSpace="nowrap"
+      >
+        About us
+      </ChakraLink>
 
-    {
-      isDrawer ? <Box
-        width='100%'
-        height='1px'
-        borderBottom='1px solid'
-        borderColor="gray.200"
-      /> : <Box flexGrow={1}/>
-    }
+      {isDrawer ? (
+        <Box
+          width="100%"
+          height="1px"
+          borderBottom="1px solid"
+          borderColor="gray.200"
+        />
+      ) : (
+        <Box flexGrow={1} />
+      )}
 
-    {
-      products.map((product) => (
+      {products.map((product) => (
         <ChakraLink
           key={product.name}
           as="button"
-          onClick={() => navigate('/app/' + product.name)}
+          onClick={() => navigate("/app/" + product.name)}
           fontSize="md"
           _hover={STYLES.linkHovered}
         >
           {product.name}
         </ChakraLink>
-      ))
-    }
+      ))}
 
-    {
-      isDrawer ? <Box
-        width='100%'
-        height='1px'
-        borderBottom='1px solid'
-        borderColor="gray.200"
-      /> : null
-    }
+      {isDrawer ? (
+        <Box
+          width="100%"
+          height="1px"
+          borderBottom="1px solid"
+          borderColor="gray.200"
+        />
+      ) : null}
 
-    {
-      token ?
+      {token ? (
         <ChakraLink
           as="button"
-          onClick={() => navigate('/dashboard')}
+          onClick={() => navigate("/dashboard")}
           fontSize="md"
-          style={{ display: 'flex', alignItems: 'center' }}
+          style={{ display: "flex", alignItems: "center" }}
           _hover={STYLES.linkHovered}
         >
           <Box
             boxSize={6}
             marginRight={2}
-            border={isDrawer ? '1px solid #1A202C' : '1px solid white'}
+            border={isDrawer ? "1px solid #1A202C" : "1px solid white"}
             borderRadius={50}
-            textAlign='center'
-            display='flex'
-            alignItems='center'
+            textAlign="center"
+            display="flex"
+            alignItems="center"
             justifyContent="center"
           >
-            <FaUser/>
+            <FaUser />
           </Box>
           Dashboard
-        </ChakraLink> :
+        </ChakraLink>
+      ) : (
         <ChakraLink
           as="button"
           onClick={() => onOpen()}
           fontSize="md"
-          style={{ display: 'flex', alignItems: 'center' }}
+          style={{ display: "flex", alignItems: "center" }}
           _hover={STYLES.linkHovered}
         >
           Login
         </ChakraLink>
-    }
-    <Help isDrawer={isDrawer}/>
+      )}
+      <Help isDrawer={isDrawer} />
 
-    {
-      isDrawer ? <Box
-        width='100%'
-        height='1px'
-      /> : null
-    }
-  </>
-}
-const Navbar: React.FC = () => {
-  const navigate = useNavigate();
-  const dispatch: AppDispatch = useDispatch();
-  const { products } = useSelector(
-    (state: RootState) => state.products
+      {isDrawer ? <Box width="100%" height="1px" /> : null}
+    </>
   );
+};
+const Navbar: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { token } = useSelector((state: RootState) => state.auth);
   const {
     isOpen: isDrawerOpen,
     onOpen: onDrawerOpen,
-    onClose: onDrawerClose
+    onClose: onDrawerClose,
   } = useDisclosure();
 
   useEffect(() => {
@@ -159,37 +149,39 @@ const Navbar: React.FC = () => {
   }, [dispatch]);
 
   return (
-    <Box top={0}
-         width="100%" as="nav"
-         padding="10px 20px"
-         bg="gray.500"
-         zIndex={1000}
-         textColor={"white"}
+    <Box
+      top={0}
+      width="100%"
+      as="nav"
+      padding="10px 20px"
+      bg="gray.500"
+      zIndex={1000}
+      textColor={"white"}
     >
       <Container
-        maxW='container.xl'
+        maxW="container.xl"
         textAlign="center"
         bg="transparent"
         paddingX={{ base: 0, lg: 4 }}
       >
         <Flex justify="space-between" align="center">
-          <HStack spacing="24px" marginRight='24px'>
+          <HStack spacing="24px" marginRight="24px">
             <ChakraLink
               as={RouterLink}
               to="/"
               fontSize="md"
               _hover={STYLES.linkHovered}
-              display='flex'
+              display="flex"
               style={{
                 fontWeight: 900,
                 fontSize: "1.125rem",
-                padding: 0
+                padding: 0,
               }}
             >
               <Image
-                src='/static/images/logos/geohosting-full-white.svg'
-                alt='Kartoza Logo'
-                style={{ 'cursor': 'pointer' }}
+                src="/static/images/logos/geohosting-full-white.svg"
+                alt="Kartoza Logo"
+                style={{ cursor: "pointer" }}
                 mr={2}
               />
             </ChakraLink>
@@ -197,40 +189,38 @@ const Navbar: React.FC = () => {
 
           <HStack
             spacing="24px"
-            display={{ base: 'none', lg: 'flex' }}
+            display={{ base: "none", lg: "flex" }}
             marginLeft="auto"
             flexGrow={1}
           >
-            <NavbarContent onOpen={onOpen} isDrawer={false}/>
+            <NavbarContent onOpen={onOpen} isDrawer={false} />
           </HStack>
 
           <IconButton
             aria-label="Open menu"
-            icon={<HamburgerIcon/>}
-            display={{ base: 'flex', lg: 'none' }}
-            backgroundColor={'gray.500'}
-            _hover={{ backgroundColor: 'gray.500' }}
+            icon={<HamburgerIcon />}
+            display={{ base: "flex", lg: "none" }}
+            backgroundColor={"gray.500"}
+            _hover={{ backgroundColor: "gray.500" }}
             onClick={onDrawerOpen}
           />
         </Flex>
 
-        <Drawer
-          isOpen={isDrawerOpen} placement="right" onClose={onDrawerClose}
-        >
-          <DrawerOverlay/>
+        <Drawer isOpen={isDrawerOpen} placement="right" onClose={onDrawerClose}>
+          <DrawerOverlay />
           <DrawerContent>
-            <DrawerCloseButton/>
+            <DrawerCloseButton />
             <DrawerHeader></DrawerHeader>
 
             <DrawerBody>
               <VStack spacing="24px" align="start">
-                <NavbarContent onOpen={onOpen} isDrawer={true}/>
+                <NavbarContent onOpen={onOpen} isDrawer={true} />
               </VStack>
             </DrawerBody>
           </DrawerContent>
         </Drawer>
 
-        <LoginForm isOpen={isOpen} onClose={onClose}/>
+        <LoginForm isOpen={isOpen} onClose={onClose} />
       </Container>
     </Box>
   );
