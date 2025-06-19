870b8b6e5809c20a12a8d65703832c5e2475f016
Irwan Fathurrahman <meomancer@gmail.com>
2025-06-19 09:45:19 +0700

    Add form to make keepass detect form

--- django_project/geohosting/src/components/LoginForm/LoginForm.tsx
+++ django_project/geohosting/src/components/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Button,
   FormControl,
@@ -16,43 +16,46 @@ import {
   ModalOverlay,
   Text,
   VStack,
-} from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { toast } from 'react-toastify';
-import { AppDispatch, RootState } from '../../redux/store';
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { AppDispatch, RootState } from "../../redux/store";
 import {
   login,
   logout,
   register,
-  resetPassword
-} from '../../redux/reducers/authSlice';
+  resetPassword,
+} from "../../redux/reducers/authSlice";
 
 interface LoginFormProps {
   isOpen: boolean;
   onClose: () => void;
-  formType?: 'login' | 'signup' | 'forgotPassword';
+  formType?: "login" | "signup" | "forgotPassword";
   onSuccess?: () => void;
 }
 
-const LoginForm: React.FC<LoginFormProps> = (
-  { isOpen, onClose, formType = 'login', onSuccess }
-) => {
+const LoginForm: React.FC<LoginFormProps> = ({
+  isOpen,
+  onClose,
+  formType = "login",
+  onSuccess,
+}) => {
   const dispatch: AppDispatch = useDispatch();
   const navigate = useNavigate();
-  const {
-    token,
-    loading,
-    error
-  } = useSelector((state: RootState) => state.auth);
+  const { token, loading, error } = useSelector(
+    (state: RootState) => state.auth,
+  );
   const [show, setShow] = useState(false);
-  const [email, setEmail] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
   const [isEmailValid, setIsEmailValid] = useState<boolean>(true);
-  const [password, setPassword] = useState<string>('');
-  const [firstName, setFirstName] = useState<string>('');
-  const [lastName, setLastName] = useState<string>('');
+  const [password, setPassword] = useState<string>("");
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
   const [isSignUp, setIsSignUp] = useState<boolean>(false);
-  const [isForgotPassword, setIsForgotPassword] = useState<boolean>(formType === 'forgotPassword');
+  const [isForgotPassword, setIsForgotPassword] = useState<boolean>(
+    formType === "forgotPassword",
+  );
   const [isEmailTouched, setIsEmailTouched] = useState<boolean>(false);
 
   const handleClick = () => setShow(!show);
@@ -66,44 +69,50 @@ const LoginForm: React.FC<LoginFormProps> = (
     const emailValue = e.target.value;
     setEmail(emailValue);
     setIsEmailTouched(true);
-    setIsEmailValid(emailValue === '' || validateEmail(emailValue));
+    setIsEmailValid(emailValue === "" || validateEmail(emailValue));
   };
 
   const handleLogin = () => {
     dispatch(login({ email, password })).then((result: any) => {
-      if (result.meta.requestStatus === 'fulfilled') {
+      if (result.meta.requestStatus === "fulfilled") {
         onClose();
         if (!onSuccess) {
           location.reload();
         } else {
-          onSuccess()
+          onSuccess();
         }
-      } else if (result.meta.requestStatus === 'rejected') {
+      } else if (result.meta.requestStatus === "rejected") {
         const errorMessages = result.payload
-          ? Object.entries(result.payload).map(([key, value]) => `${value}`).join('\n')
-          : 'Login failed. Please try again.';
+          ? Object.entries(result.payload)
+              .map(([key, value]) => `${value}`)
+              .join("\n")
+          : "Login failed. Please try again.";
         toast.error(errorMessages);
       }
     });
   };
 
   const handleSignUp = () => {
-    dispatch(register({
-      email,
-      password,
-      firstName,
-      lastName
-    })).then((result: any) => {
-      if (result.meta.requestStatus === 'fulfilled') {
+    dispatch(
+      register({
+        email,
+        password,
+        firstName,
+        lastName,
+      }),
+    ).then((result: any) => {
+      if (result.meta.requestStatus === "fulfilled") {
         if (!onSuccess) {
           location.reload();
         } else {
-          onSuccess()
+          onSuccess();
         }
-      } else if (result.meta.requestStatus === 'rejected') {
+      } else if (result.meta.requestStatus === "rejected") {
         const errorMessages = result.payload
-          ? Object.entries(result.payload).map(([key, value]) => `${value}`).join('\n')
-          : 'Sign up failed. Please try again.';
+          ? Object.entries(result.payload)
+              .map(([key, value]) => `${value}`)
+              .join("\n")
+          : "Sign up failed. Please try again.";
         toast.error(errorMessages);
       }
     });
@@ -111,16 +120,16 @@ const LoginForm: React.FC<LoginFormProps> = (
 
   const handleLogout = () => {
     dispatch(logout()).then(() => {
-      toast.success('Successfully logged out.');
+      toast.success("Successfully logged out.");
       onClose();
     });
   };
 
   const handleKeyPress = (e: React.KeyboardEvent) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       if (isForgotPassword) {
         handlePasswordReset();
-      } else if (isSignUp || formType === 'signup') {
+      } else if (isSignUp || formType === "signup") {
         handleSignUp();
       } else {
         handleLogin();
@@ -130,20 +139,22 @@ const LoginForm: React.FC<LoginFormProps> = (
 
   const handlePasswordReset = () => {
     dispatch(resetPassword(email)).then((result: any) => {
-      if (result.meta.requestStatus === 'fulfilled') {
-        toast.success('An email has been sent to reset your password.');
+      if (result.meta.requestStatus === "fulfilled") {
+        toast.success("An email has been sent to reset your password.");
         setIsForgotPassword(false);
-      } else if (result.meta.requestStatus === 'rejected') {
+      } else if (result.meta.requestStatus === "rejected") {
         const errorMessages = result.payload
-          ? Object.entries(result.payload).map(([key, value]) => `${value}`).join('\n')
-          : 'Password reset failed. Please try again later.';
+          ? Object.entries(result.payload)
+              .map(([key, value]) => `${value}`)
+              .join("\n")
+          : "Password reset failed. Please try again later.";
         toast.error(errorMessages);
       }
     });
   };
 
   const isFormValid = () => {
-    if (isSignUp || formType === 'signup') {
+    if (isSignUp || formType === "signup") {
       return email && password && firstName && lastName && isEmailValid;
     } else if (isForgotPassword) {
       return email && isEmailValid;
@@ -154,142 +165,167 @@ const LoginForm: React.FC<LoginFormProps> = (
   const handleBackToLogin = () => {
     setIsForgotPassword(false);
     setIsEmailTouched(false);
-    setEmail('');
+    setEmail("");
   };
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} blockScrollOnMount={true}
-           preserveScrollBarGap={true}>
-      <ModalOverlay/>
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      blockScrollOnMount={true}
+      preserveScrollBarGap={true}
+    >
+      <ModalOverlay />
       <ModalContent>
         <ModalHeader>
           {token
-            ? 'Welcome'
-            : isSignUp || formType === 'signup'
-              ? 'Sign Up'
+            ? "Welcome"
+            : isSignUp || formType === "signup"
+              ? "Sign Up"
               : isForgotPassword
-                ? 'Reset Password'
-                : 'Log in'}
+                ? "Reset Password"
+                : "Log in"}
         </ModalHeader>
-        <ModalCloseButton/>
+        <ModalCloseButton />
         <ModalBody>
           {token ? (
             <Text>You are already logged in.</Text>
           ) : (
-            <VStack spacing={4}>
-              {(isSignUp || formType === 'signup') && (
-                <>
-                  <FormControl id="first-name" isRequired>
-                    <FormLabel>First Name</FormLabel>
-                    <Input
-                      type="text"
-                      placeholder="Enter first name"
-                      value={firstName}
-                      onChange={(e) => setFirstName(e.target.value)}
-                      onKeyUp={handleKeyPress}
-                    />
-                  </FormControl>
-                  <FormControl id="last-name" isRequired>
-                    <FormLabel>Last Name</FormLabel>
-                    <Input
-                      type="text"
-                      placeholder="Enter last name"
-                      value={lastName}
-                      onChange={(e) => setLastName(e.target.value)}
-                      onKeyUp={handleKeyPress}
-                    />
-                  </FormControl>
-                </>
-              )}
-              <FormControl id="email" isRequired>
-                <FormLabel>Email</FormLabel>
-                <Input
-                  type="email"
-                  placeholder="Enter email"
-                  value={email}
-                  autoFocus={true}
-                  onChange={handleEmailChange}
-                  onKeyUp={handleKeyPress}
-                />
-                {isEmailTouched && !isEmailValid &&
-                  <Text color="red.500">Invalid email address.</Text>}
-              </FormControl>
-              {!isForgotPassword && (
-                <FormControl id="password" isRequired>
-                  <FormLabel>Password</FormLabel>
-                  <InputGroup size="md">
-                    <Input
-                      pr="4.5rem"
-                      type={show ? 'text' : 'password'}
-                      placeholder="Enter password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      onKeyUp={handleKeyPress}
-                    />
-                    <InputRightElement width="4.5rem">
-                      <Button
-                        h="1.75rem"
-                        size="sm"
-                        onClick={handleClick}
-                        colorScheme="blue"
-                      >
-                        {show ? 'Hide' : 'Show'}
-                      </Button>
-                    </InputRightElement>
-                  </InputGroup>
+            <form>
+              <VStack spacing={4}>
+                {(isSignUp || formType === "signup") && (
+                  <>
+                    <FormControl id="first-name" isRequired>
+                      <FormLabel>First Name</FormLabel>
+                      <Input
+                        type="text"
+                        placeholder="Enter first name"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        onKeyUp={handleKeyPress}
+                      />
+                    </FormControl>
+                    <FormControl id="last-name" isRequired>
+                      <FormLabel>Last Name</FormLabel>
+                      <Input
+                        type="text"
+                        placeholder="Enter last name"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        onKeyUp={handleKeyPress}
+                      />
+                    </FormControl>
+                  </>
+                )}
+                <FormControl id="email" isRequired>
+                  <FormLabel>Email</FormLabel>
+                  <Input
+                    type="email"
+                    placeholder="Enter email"
+                    value={email}
+                    autoFocus={true}
+                    onChange={handleEmailChange}
+                    onKeyUp={handleKeyPress}
+                  />
+                  {isEmailTouched && !isEmailValid && (
+                    <Text color="red.500">Invalid email address.</Text>
+                  )}
                 </FormControl>
-              )}
-              {!isForgotPassword && formType !== 'signup' && (
-                <Link
-                  href="#"
-                  fontSize="sm"
-                  color="purple.500"
-                  display="block"
-                  alignSelf="flex-end"
-                  onClick={(e) => {
-                    e.preventDefault(); // Prevent page reload
-                    setIsForgotPassword(true);
-                  }}
-                >
-                  Forgot your password?
-                </Link>
-              )}
-            </VStack>
+                {!isForgotPassword && (
+                  <FormControl id="password" isRequired>
+                    <FormLabel>Password</FormLabel>
+                    <InputGroup size="md">
+                      <Input
+                        pr="4.5rem"
+                        type={show ? "text" : "password"}
+                        placeholder="Enter password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        onKeyUp={handleKeyPress}
+                      />
+                      <InputRightElement width="4.5rem">
+                        <Button
+                          h="1.75rem"
+                          size="sm"
+                          onClick={handleClick}
+                          colorScheme="blue"
+                        >
+                          {show ? "Hide" : "Show"}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                  </FormControl>
+                )}
+                {!isForgotPassword && formType !== "signup" && (
+                  <Link
+                    href="#"
+                    fontSize="sm"
+                    color="purple.500"
+                    display="block"
+                    alignSelf="flex-end"
+                    onClick={(e) => {
+                      e.preventDefault(); // Prevent page reload
+                      setIsForgotPassword(true);
+                    }}
+                  >
+                    Forgot your password?
+                  </Link>
+                )}
+              </VStack>
+            </form>
           )}
         </ModalBody>
         <ModalFooter justifyContent="center" flexDirection="column">
           {token ? (
-            <Button colorScheme="blue" onClick={handleLogout}
-                    isLoading={loading}>
+            <Button
+              colorScheme="blue"
+              onClick={handleLogout}
+              isLoading={loading}
+            >
               Logout
             </Button>
           ) : (
             <>
               <Button
                 colorScheme="blue"
-                onClick={isForgotPassword ? handlePasswordReset : (isSignUp || formType === 'signup') ? handleSignUp : handleLogin}
+                onClick={
+                  isForgotPassword
+                    ? handlePasswordReset
+                    : isSignUp || formType === "signup"
+                      ? handleSignUp
+                      : handleLogin
+                }
                 isLoading={loading}
                 isDisabled={!isFormValid()}
               >
-                {isForgotPassword ? 'Reset Password' : (isSignUp || formType === 'signup') ? 'Sign Up' : 'Login'}
+                {isForgotPassword
+                  ? "Reset Password"
+                  : isSignUp || formType === "signup"
+                    ? "Sign Up"
+                    : "Login"}
               </Button>
-              {
-                !isForgotPassword && formType !== 'signup' && (
-                  <Button variant="ghost" size="sm" mt={2}
-                          onClick={() => setIsSignUp(!isSignUp)}>
-                    {isSignUp ? 'Have an account? Log in' : 'Need an account? Sign up'}
-                  </Button>
-                )
-              }
-              {
-                isForgotPassword && (
-                  <Button
-                    variant="ghost" size="sm" mt={2}
-                    onClick={handleBackToLogin}>
-                    Back to Login
-                  </Button>
-                )
-              }
+              {!isForgotPassword && formType !== "signup" && (
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  mt={2}
+                  onClick={() => setIsSignUp(!isSignUp)}
+                >
+                  {isSignUp
+                    ? "Have an account? Log in"
+                    : "Need an account? Sign up"}
+                </Button>
+              )}
+              {isForgotPassword && (
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  mt={2}
+                  onClick={handleBackToLogin}
+                >
+                  Back to Login
+                </Button>
+              )}
             </>
           )}
         </ModalFooter>
