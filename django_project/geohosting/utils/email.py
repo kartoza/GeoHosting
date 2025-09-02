85f12e545a91d4fc50aeceea29df4f02413caa58
Irwan Fathurrahman <meomancer@gmail.com>
2025-09-02 09:44:07 +0700

    Updates

--- django_project/geohosting/utils/email.py
+++ django_project/geohosting/utils/email.py
@@ -13,7 +13,6 @@ from django.utils import timezone
 from core.models.preferences import Preferences
 from core.settings.base import FRONTEND_URL
 from geohosting_event.models.email import EmailEvent, EmailCategory
-from geohosting_event.models.log import LogTracker
 
 
 class InstanceEmail:
@@ -36,42 +35,22 @@ class InstanceEmail:
             return
         pref = Preferences.load()
         name = f'{instance.owner.first_name} {instance.owner.last_name}'
-        if not instance.vault_url:
-            html_content = render_to_string(
-                template_name='emails/GeoHosting_Product is Error.html',
-                context={
-                    'name': name,
-                }
-            )
-        else:
-            try:
-                instance.credential()
-                instance_url = (
-                    f"{FRONTEND_URL}#/dashboard?q={instance.name}"
-                )
-                instance_url = instance_url.replace('#/#', '#')
-                html_content = render_to_string(
-                    template_name='emails/GeoHosting_Product is Ready.html',
-                    context={
-                        'name': name,
-                        'url': instance.url,
-                        'instance_url': instance_url,
-                        'app_name': instance.name,
-                        'support_email': pref.support_email,
-                    }
-                )
-                LogTracker.success(self.instance, 'Get credential')
-            except Exception as e:
-                LogTracker.error(self.instance, f'Get credential : {e}')
-                html_content = render_to_string(
-                    template_name='emails/GeoHosting_Product is Error.html',
-                    context={
-                        'name': name,
-                        'app_name': instance.name,
-                        'url': instance.url,
-                        'support_email': pref.support_email,
-                    }
-                )
+
+        # Send email that instance is ready
+        instance_url = (
+            f"{FRONTEND_URL}#/dashboard?q={instance.name}"
+        )
+        instance_url = instance_url.replace('#/#', '#')
+        html_content = render_to_string(
+            template_name='emails/GeoHosting_Product is Ready.html',
+            context={
+                'name': name,
+                'url': instance.url,
+                'instance_url': instance_url,
+                'app_name': instance.name,
+                'support_email': pref.support_email,
+            }
+        )
 
         # Create the email message
         EmailEvent.send_email(
