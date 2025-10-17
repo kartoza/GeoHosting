d958d43e7ac76fa7f3d56038d061912da2d1fdcd
Irwan Fathurrahman <meomancer@gmail.com>
2025-10-17 11:18:37 +0700

    Add exception catcher

--- django_project/geohosting/tasks/coupon.py
+++ django_project/geohosting/tasks/coupon.py
@@ -1,5 +1,10 @@
+import logging
+
 from core.celery import app
 from geohosting.models import Coupon
+from geohosting_event.models import LogTracker
+
+logger = logging.getLogger(__name__)
 
 
 @app.task(name='sync_coupon')
@@ -7,7 +12,13 @@ def sync_stripe_coupon(id):
     """Sync stripe coupon."""
     try:
         coupon = Coupon.objects.get(id=id)
+        coupon.sync_stripe()
         for coupon_code in coupon.couponcode_set.all():
-            coupon_code.sync_stripe()
+            try:
+                coupon_code.sync_stripe()
+            except Exception as e:
+                LogTracker.error(coupon, f'{e}')
     except Coupon.DoesNotExist:
         pass
+    except Exception as e:
+        logger.error(e)
