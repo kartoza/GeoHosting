160df419476290aba92a8cc382b27b0fb1ea8a07
Irwan Fathurrahman <meomancer@gmail.com>
2026-04-15 10:56:07 +0700

    Upgrade django

--- django_project/core/settings/project.py
+++ django_project/core/settings/project.py
@@ -12,12 +12,14 @@ ALLOWED_HOSTS = ['*']
 ADMINS = ()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': os.environ.get(
+            'DATABASE_ENGINE', 'django.contrib.gis.db.backends.postgis'
+        ),
         'NAME': os.environ['DATABASE_NAME'],
         'USER': os.environ['DATABASE_USERNAME'],
         'PASSWORD': os.environ['DATABASE_PASSWORD'],
         'HOST': os.environ['DATABASE_HOST'],
-        'PORT': 5432,
+        'PORT': os.environ.get('DATABASE_PORT', '5432'),
         'TEST_NAME': 'unittests',
         'TEST': {
             'NAME': 'unittests',
